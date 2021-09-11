0628983627200a6a46f2597b50c7160073028597
xNoJustice <harunmimarsinan@gmail.com>
2021-09-11 20:18:26 +0300

    changes on logo and lint

--- src/App.js
+++ src/App.js
@@ -6,7 +6,7 @@ function App() {
   return (
     <div className="w-full h-screen flex justify-center items-center mx-auto p-6 dark:text-white text-2xl font-bold">
       <div>
-        <img src={logo} alt="logo" className="logo" />
+        <img src={logo} alt="logo" className="w-32 h-32 mx-auto logo" />
         Welcome to my App
       </div>
     </div>
